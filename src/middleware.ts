7acdff06e799e158f567236f2219267cc51cc000
JSegovia <acosta.segoviaa@gmail.com>
2022-10-26 23:15:33 -0500

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,15 +6,14 @@ import { isAdminRole, isSuperAdminRole, isValidRole } from './helpers';
 
 export async function middleware(req: NextRequest) {
   const jwt = req.cookies.get('token');
-  const { origin } = req.nextUrl;
 
   if (!jwt) {
     if (req.nextUrl.pathname.startsWith('/admin')) {
-      return NextResponse.redirect(`${origin}/auth/inicia-sesion`);
+      return NextResponse.redirect(new URL('/auth/inicia-sesion', req.url));
     }
 
     if (req.nextUrl.pathname.startsWith('/perfil')) {
-      return NextResponse.redirect(`${origin}/auth/inicia-sesion`);
+      return NextResponse.redirect(new URL('/auth/inicia-sesion', req.url));
     }
 
     return NextResponse.next();
@@ -23,54 +22,54 @@ export async function middleware(req: NextRequest) {
   const { payload }: any = await jwtVerify(jwt, new TextEncoder().encode(JWT));
 
   if (req.nextUrl.pathname.startsWith('/auth')) {
-    return NextResponse.redirect(`${origin}/`);
+    return NextResponse.redirect(new URL('/', req.url));
   }
 
   if (req.nextUrl.pathname.startsWith('/perfil')) {
     if (!payload.user.role_id) {
-      return NextResponse.redirect(`${origin}/`);
+      return NextResponse.redirect(new URL('/', req.url));
     }
   }
 
   if (req.nextUrl.pathname.startsWith('/admin/variantes')) {
     if (!isSuperAdminRole(payload.user.role_id)) {
-      return NextResponse.redirect(`${origin}/admin`);
+      return NextResponse.redirect(new URL('/admin', req.url));
     }
   }
 
   if (req.nextUrl.pathname.startsWith('/admin/codigos')) {
     if (!isSuperAdminRole(payload.user.role_id)) {
-      return NextResponse.redirect(`${origin}/admin`);
+      return NextResponse.redirect(new URL('/admin', req.url));
     }
   }
 
   if (req.nextUrl.pathname.startsWith('/admin/comentarios')) {
     if (!isAdminRole(payload.user.role_id)) {
-      return NextResponse.redirect(`${origin}/admin`);
+      return NextResponse.redirect(new URL('/admin', req.url));
     }
   }
 
   if (req.nextUrl.pathname.startsWith('/admin/direcciones')) {
     if (!isAdminRole(payload.user.role_id)) {
-      return NextResponse.redirect(`${origin}/admin`);
+      return NextResponse.redirect(new URL('/admin', req.url));
     }
   }
 
   if (req.nextUrl.pathname.startsWith('/admin/ratings')) {
     if (!isAdminRole(payload.user.role_id)) {
-      return NextResponse.redirect(`${origin}/admin`);
+      return NextResponse.redirect(new URL('/admin', req.url));
     }
   }
 
   if (req.nextUrl.pathname.startsWith('/admin/usuarios')) {
     if (!isAdminRole(payload.user.role_id)) {
-      return NextResponse.redirect(`${origin}/admin`);
+      return NextResponse.redirect(new URL('/admin', req.url));
     }
   }
 
   if (req.nextUrl.pathname.startsWith('/admin')) {
     if (!isValidRole(payload.user.role_id)) {
-      return NextResponse.redirect(`${origin}/`);
+      return NextResponse.redirect(new URL('/', req.url));
     }
   }
 
