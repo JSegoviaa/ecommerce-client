51dc551e87fd96d3ab405eb9a43a78b8af6fd7d5
JSegovia <acosta.segoviaa@gmail.com>
2022-10-27 17:10:57 -0500

    se agrega token headers

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import { API, API_TOKEN } from '../env';
+
+const token = Cookies.get('token') as string;
 
 const api = axios.create({
   baseURL: API,
-  headers: { 'api-token': API_TOKEN },
+  headers: { 'api-token': API_TOKEN, 'x-token': token },
   withCredentials: true,
 });
 
